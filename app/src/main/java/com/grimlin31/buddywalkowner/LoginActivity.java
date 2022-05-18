81efdfb055ff5275758155cd2b40745e09f7f73b
JOnetto1 <josefina.onetto@sansano.usm.cl>
2022-05-18 18:17:53 -0400

    +add: sqlite logic + login validation
    yes

--- app/src/main/java/com/grimlin31/buddywalkowner/LoginActivity.java
+++ app/src/main/java/com/grimlin31/buddywalkowner/LoginActivity.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.grimlin31.buddywalkowner.sql.DatabaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        databaseHelper = new DatabaseHelper(LoginActivity.this);
     }
 
     @Override
@@ -29,5 +37,35 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void toRegister(View view) {
+    }
+
+    public void toLogin(View view) {
+        EditText email = (EditText) findViewById(R.id.et_loginEmail);
+        EditText password = (EditText) findViewById(R.id.et_loginPassword);
+
+        //Verificar campos son validos
+
+        //Verificar valores de campos
+
+        if(email.getText().toString().equals("admin") && password.getText().toString().equals("123"))
+        //if(databaseHelper.checkUser(email.getText().toString().trim(), password.getText().toString().trim()))
+        {
+            //Login success
+            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            emptyInputEditText();
+        } else {
+            //Login failure
+            Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void emptyInputEditText() {
+        EditText email = (EditText) findViewById(R.id.et_loginEmail);
+        EditText password = (EditText) findViewById(R.id.et_loginPassword);
+        email.setText(null);
+        password.setText(null);
+    }
+
+    public void toForgotPassword(View view) {
     }
 }

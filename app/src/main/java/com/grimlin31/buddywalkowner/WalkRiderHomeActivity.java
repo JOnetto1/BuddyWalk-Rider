cc01d7b66ef6ade09cfaca02168a704b2014ee74
Ariel Zuñiga <ariel.zunigar@sansano.usm.cl>
2022-05-27 20:19:33 -0400

    interfaz paseador

---
+++ app/src/main/java/com/grimlin31/buddywalkowner/WalkRiderHomeActivity.java
@@ -0,0 +1,67 @@
+package com.grimlin31.buddywalkowner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.grimlin31.buddywalkowner.FragmentContainer.FirstFragment;
+import com.grimlin31.buddywalkowner.FragmentContainer.SecondFragment;
+import com.grimlin31.buddywalkowner.FragmentContainer.ThirdFragment;
+
+public class WalkRiderHomeActivity extends AppCompatActivity {
+
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walk_rider_home);
+
+        BottomNavigationView navigation = findViewById(R.id.home_navigator);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(secondFragment);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()){
+                case R.id.firstFragment:
+                    loadFragment(firstFragment);
+                    return true;
+                case R.id.secondFragment:
+                    loadFragment(secondFragment);
+                    return true;
+                case R.id.thirdFragment:
+                    loadFragment(thirdFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_home, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
